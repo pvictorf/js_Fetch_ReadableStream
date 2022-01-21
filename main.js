e871ea432e125a5a52f591b6f8ecdb6655e6897b
pvictorf <41065334+pvictorf@users.noreply.github.com>
2022-01-21 14:33:46 -0300

    Add log for percentage

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const loadingEl = document.querySelector("#loading")
 const audioUrl  = 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3'; // './src/storage/audio.mp3'
 
 AudioService.fetchAudioStream(audioUrl, {
-  onProgress: ({percentage}) => loadingEl.innerHTML += `<p>${percentage}%</p>`,
-  onFinished: ({percentage}) => loadingEl.innerHTML += `<p>Finished!</p>`,
+  onProgress: ({percentage}) => console.log(`Downloading... ${percentage}%`),
+  onFinished: ({percentage}) => console.log('Finished!'),
 }).then(source => {
     AudioService.playAudio(source)
 });
